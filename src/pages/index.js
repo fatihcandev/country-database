91fa87ccfb6124bc5184654b94aee20bbf954276
Fatih <fatihcan.social@gmail.com>
2020-07-25 19:58:25 +0300

    add key to country card

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,14 @@ import SearchBar from '../components/SearchBar';
 import Title from '../components/Title';
 import RegionFilter from '../components/RegionFilter';
 import layoutStyles from '../components/Layout/layout.module.scss';
+import Loading from '../components/Loading';
 
 const HomePage = () => {
   const [searchInput, setSearchInput] = useState("");
   const [regionFilter, setRegionFilter] = useState("");
   const { loading, error, data } = useQuery(query);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <Loading />
   if (error) return <p>Error</p>;
 
   return (
@@ -25,36 +26,42 @@ const HomePage = () => {
       </div>
       <div className={layoutStyles.cards}>
         {
-          searchInput
-            ? data.countries
-              .filter(country => country.name.toLowerCase().includes(searchInput))
-              .map(({ name, population, region, capital, flag }) => (
-                <CountryCard
-                  name={name}
-                  population={population}
-                  region={region}
-                  capital={capital}
-                  flag={flag} />
-              ))
-            : regionFilter
-              ? data.countries
-                .filter(country => country.region === regionFilter)
-                .map(({ name, population, region, capital, flag }) => (
-                  <CountryCard
-                    name={name}
-                    population={population}
-                    region={region}
-                    capital={capital}
-                    flag={flag} />
-                ))
-              : data.countries.map(({ name, population, region, capital, flag }) => (
-                <CountryCard
-                  name={name}
-                  population={population}
-                  region={region}
-                  capital={capital}
-                  flag={flag} />
-              ))
+          !searchInput && !regionFilter && data.countries
+            .map(({ name, population, region, capital, flag }) => (
+              <CountryCard
+                key={name}
+                name={name}
+                population={population}
+                region={region}
+                capital={capital}
+                flag={flag} />
+            ))
+        }
+        {
+          searchInput && data.countries
+            .filter(country => country.name.toLowerCase().includes(searchInput))
+            .map(({ name, population, region, capital, flag }) => (
+              <CountryCard
+                key={name}
+                name={name}
+                population={population}
+                region={region}
+                capital={capital}
+                flag={flag} />
+            ))
+        }
+        {
+          regionFilter && data.countries
+            .filter(country => country.region === regionFilter)
+            .map(({ name, population, region, capital, flag }) => (
+              <CountryCard
+                key={name}
+                name={name}
+                population={population}
+                region={region}
+                capital={capital}
+                flag={flag} />
+            ))
         }
 
       </div>
