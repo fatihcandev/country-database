4b68f1293b6bd0ccfa91ee00d18e4ef2d7b41624
Fatih <fatihcan.social@gmail.com>
2020-07-25 20:21:31 +0300

    meta image fix

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -10,13 +10,13 @@ const SEO = () => (
     <meta property="og:url" content="https://country-database.vercel.app/" />
     <meta property="og:title" content="Country Database" />
     <meta property="og:description" content="A searchable database of all the countries in the world." />
-    <meta property="og:image" content={`${__dirname}src/images/meta-images.jpg`} />
+    <meta property="og:image" content={`src/images/meta-images.jpg`} />
 
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content="https://country-database.vercel.app/" />
     <meta property="twitter:title" content="Country Database" />
     <meta property="twitter:description" content="A searchable database of all the countries in the world." />
-    <meta property="twitter:image" content={`${__dirname}src/images/meta-images.jpg`} />
+    <meta property="twitter:image" content={`src/images/meta-images.jpg`} />
   </Head>
 )
 
