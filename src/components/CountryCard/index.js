59af1f49a57fe466216c79de40fe1d3577a008c0
Fatih <fatihcan.social@gmail.com>
2020-07-25 19:58:11 +0300

    add key prop

--- src/components/CountryCard/index.js
+++ src/components/CountryCard/index.js
@@ -1,8 +1,8 @@
 import styles from './countryCard.module.scss';
 import CountryCardContent from '../CountryCardContent';
 
-const CountryCard = ({ name, population, region, capital, flag }) => (
-  <div className={styles.wrapper}>
+const CountryCard = ({ name, population, region, capital, flag, key }) => (
+  <div className={styles.wrapper} key={key}>
     <img loading="lazy" src={flag} alt={`flag of ${name}`} className={styles.flag} />
     <div className={styles.content}>
       <h2 className={styles.name}>{name}</h2>
